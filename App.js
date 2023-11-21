c1bd112b82886a1440180059f9ce6caa8860e773
Pratik Kumar <pratikkumarsbg@gmail.com>
2023-11-21 11:19:40 +0530

    igniting the app with parcel

--- App.js
+++ App.js
@@ -1,25 +1,28 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", abc: "xyz" },
-  "Hello World from React!"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading", abc: "xyz" },
+//   "Hello World from React!"
+// );
 
 // Nested HTML using React
 const parent = React.createElement("div", {}, [
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h1", {}, "This is React Dev 🚀"),
     React.createElement("h2", {}, "I am a h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag"),
+    React.createElement("h1", {}, "This is Second React Dev"),
     React.createElement("h2", {}, "I am a h2 tag"),
   ]),
 ]);
 
-console.log(heading); // returns an object
-console.log(parent); // returns an object
+// console.log(heading); // returns an object
+// console.log(parent); // returns an object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
+// console.log(root);
 
 root.render(parent);
---
+++ readme.md
@@ -0,0 +1,20 @@
+# React-Dev 🚀
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support for old browsers
+- Diagnostics
+- Error Handling
+- Tree Shaking - removes unused codes
+- Differential Prod. and Dev Bundles
